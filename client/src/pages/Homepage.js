71b1b379517e4f72216a9080fb956e94de6e7a5c
Alejandra Barrero <lala@MacBook-Pro-2.local>
2023-05-13 12:28:56 -0400

    Got backend server up and running and added navigation bar

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -9,7 +9,11 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
   
 
 // import ProfileList from '../components/ProfileList';
@@ -19,6 +23,7 @@ import { QUERY_PROFILES } from '../utils/queries';
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
+  const navItems = ['Login', 'Sign Up'];
 
 const stories = [
   {title: "my epic story 1", summary: "suddenly words started to rain in the forrest"},
@@ -26,8 +31,41 @@ const stories = [
   {title: "my comedy 1", summary: "suddenly words started to rain at the doctors"}
 ]
 
+const loadAuthpage = (event) => {
+  event.preventDefault()
+  console.log (event.target)
+}
+
   return (
-    <Grid container spacing={2}>
+    <>
+     <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={loadAuthpage}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={2}>
     {stories.map((story, idx)=>(
        <Grid item xs={4}>
       <Card sx={{ maxWidth: 345 }}>
@@ -52,6 +90,8 @@ const stories = [
     </Grid>
     ))}
     </Grid>
+    </>
+    
   );
 };
 
