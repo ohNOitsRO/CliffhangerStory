e316b4f46566678b651734933b166da40834906a
PatienceAlexandre <119373546+PatienceAlexandre@users.noreply.github.com>
2023-05-17 22:50:18 -0400

    updated code

--- client/src/components/CardColumn/index.js
+++ client/src/components/CardColumn/index.js
@@ -1,15 +1,17 @@
 import Storycard from "../StoryCards"
 
 
-export default function CardColumn({ storytype, cardarray }) {
+export default function CardColumn({ cardarray }) {
 
   return (
-    <>
+    <div style={{display: "block"} 
+
+  }>
       {cardarray.map((storyobject) => {
         return <Storycard title={storyobject.title} summary={storyobject.summary} author={storyobject.author}/>
 
 
       })}
-    </>
+    </div>
   )
 }
