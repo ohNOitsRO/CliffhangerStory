99a2ae3ac2719f2a5f96787bbbc82394fba191e0
Alejandra Barrero <lala@MacBook-Pro-2.local>
2023-05-25 21:47:39 -0400

    mongooo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cliffhanger',
+  process.env.MONGODB_URI || 'mongodb+srv://ale:<Cliffhangerstory1>@cluster0.z5xj7sm.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
