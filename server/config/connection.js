1b1e2192ef9aaabbf1162710e32124cd064d24b5
Delgado <rxd718@miami.edu>
2023-08-14 14:57:35 -0400

    fixing mongoose db connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://ale:<Cliffhangerstory1>@cluster0.z5xj7sm.mongodb.net/?retryWrites=true&w=majority',
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
